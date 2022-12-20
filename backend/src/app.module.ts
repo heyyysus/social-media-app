5b8a6b964b94cc2186635639fd3e1e6cf81f1998
Jesus Velarde <jesus.velade07@gmail.com>
2022-12-19 21:42:05 -0800

    remove unecessary files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersController } from './controllers/users.controller';
 import { UserService } from './services/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UserService],
+  controllers: [UsersController],
+  providers: [UserService],
 })
 export class AppModule {}
